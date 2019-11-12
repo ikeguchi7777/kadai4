a2305aea394d6c1d72b17907585c860721232f4d
ikeguchi7777 <ikeguchi.0723@gmail.com>
2019-11-12 12:45:57 +0900

    エラーが起きていたので修正

--- src/MakeGUI.java
+++ src/MakeGUI.java
@@ -6,7 +6,7 @@ public class MakeGUI {
 		pages[1] = new SearchPanel("追加");
 		PageFrameBase pageFrame = new PageFrameBase("課題４", 1600, 900, pages);
 		pageFrame.setVisible(true);
-		LogArea.addLogLine("test");
+		LogArea.println("test");
 	}
 
 }
